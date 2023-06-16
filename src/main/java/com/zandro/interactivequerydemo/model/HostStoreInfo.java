9644293e5d5dab83cbc2fed778dca4a370130573
lizandrocaspe-win <lizandrocaspe@gmail.com>
2023-06-16 14:45:21 +0800

    add javadoc comment

--- src/main/java/com/zandro/interactivequerydemo/model/HostStoreInfo.java
+++ src/main/java/com/zandro/interactivequerydemo/model/HostStoreInfo.java
@@ -5,6 +5,13 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * 
+ * Object that contains host store info.
+ * 
+ * @author Lizandro Caspe
+ *
+ */
 @Data
 @AllArgsConstructor
 public class HostStoreInfo {
--- src/main/java/com/zandro/interactivequerydemo/model/KeyValueBean.java
+++ src/main/java/com/zandro/interactivequerydemo/model/KeyValueBean.java
@@ -3,6 +3,13 @@ package com.zandro.interactivequerydemo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * 
+ * A key-value bean.
+ * 
+ * @author Lizandro Caspe
+ *
+ */
 @Data
 @AllArgsConstructor
 public class KeyValueBean {
